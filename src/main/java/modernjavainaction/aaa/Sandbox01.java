f8ca031c2b9798b3c889f0b7c26234b2ce85f3fe
Vadim Storozhuk <vadozy@yahoo.com>
2019-03-31 19:12:28 -0400

    minor

--- src/main/java/modernjavainaction/aaa/Sandbox01.java
+++ src/main/java/modernjavainaction/aaa/Sandbox01.java
@@ -1,6 +1,7 @@
 package modernjavainaction.aaa;
 
 import java.util.*;
+import java.util.function.Function;
 
 import static java.util.Comparator.*;
 
@@ -8,15 +9,22 @@ public class Sandbox01 {
 
     public static void main(String[] args) {
 
-
-        List<Integer> l1 = new ArrayList<>(Arrays.asList(1, 2, 42, 3));
+        System.out.println("Original");
+        List<Integer> l1 = new ArrayList<>(Arrays.asList(2, 1, 42, 3));
         System.out.println(l1);
 
-        Collections.sort(l1, comparingInt(i -> i));
-
+        System.out.println("Lambda identity");
+        l1.sort(comparingInt(i -> i));
         System.out.println(l1);
 
-        System.out.println("Hello");
+        System.out.println("Function lambda reversed");
+        Function<Integer, Integer> f1 = i -> -i;
+        l1.sort(comparingInt(f1::apply));
+        System.out.println(l1);
+
+        System.out.println("Function identity");
+        l1.sort(comparingInt(Function.<Integer>identity()::apply));
+        System.out.println(l1);
 
     }
 }
